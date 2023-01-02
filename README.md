a6a5ee05b8df49e96dd195631ce9aafe55d61b7b
Vladimir Konrad <vladimirkonrad@gmail.com>
2023-01-02 18:15:30 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Gitpod-wp-template
- Template for development and testing of WordPress themes and plugins.
+# gitpod-wp-template
+
+Template for development and testing of WordPress themes and plugins.
+
+Example docker-compose to host Wordpress and phpmyadmin in a Gitpod Workspace.
+
+
+Just click the button below !
+
+
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/vladimirkonrad/gitpod-wp)
+
---
+++ my-plugin/my-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WordPress plugin for 
+ *
+ * @package           WordPress
+ * @author            Vladimir Konrad
+ * @copyright         2022 Vladimir Konrad
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       My plugin
+ * Plugin URI:        https://my-plugin.com
+ * Description:       WordPress plugin for something
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Vladimir Konrad
+ * Author URI:        https://vladimirkonrad.com
+ * Text Domain:       my-plugin
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://my-plugin.com
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	echo "Life is good.";
+	die;
+}
